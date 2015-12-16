93d82d2652c59647c5640d67c64efe4854ea8bd7
Qiao <qs371102@163.com>
2015-12-16 18:01:40 +0800

    all

---
+++ PL2303HXDSimpleTest/src/com/bfmj/handledevices/HandleDevices.java
@@ -0,0 +1,15 @@
+package com.bfmj.handledevices;
+
+public class HandleDevices {
+	
+	public static String getSelfID()
+	{
+		return "MJRobot";
+	}
+	
+	public static String getTargetID()
+	{
+		return "Master";
+	}
+
+}
